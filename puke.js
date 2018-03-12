ebabd239adf7aa3a2812046234a37430e3e45578
Elías Pintos Arís <eliaspintosaris@MacBook-Pro-de-Elias.local>
2018-03-12 19:08:05 +0100

    ready to puke

--- puke.js
+++ puke.js
@@ -1,9 +1,18 @@
-const f = () => {
-    setTimeout(() => {
-        // console.log('\x1b[1m', '\x1b[31m',4444, '\x1b[33m',3333, '\x1b[32m', 8888,  '\x1b[36m', '❤','\x1b[34m', 9999, '\x1b[35m', '❤', '\x1b[0m', '\x1b[39m') ;
-        console.log('\x1b[1m\x1b[31m❤❤❤❤❤\x1b[33m❤❤❤❤❤❤❤❤❤❤\x1b[0m');
-        console.log(`\x1b[1m\x1b[31m${'❤'}\x1b[33m${'❤'}\x1b[0m`);
-        f();
-    },42)
-};
-f();
+(function(o = {
+    'r': () => t(() => {
+            let m = o.c.reduce((pp, nn) => `${pp}\x1b[${nn}m█████`, '');
+            let p = o.s.join('');
+            console.log(`${p}\x1b[1m${m}\x1b[0m\x1b[39m`);
+            o.r();
+        }, 26),
+    'g': () => t(()=> {
+            let v = Math.floor(Math.random() * (10000 - 1)) + 1;
+            v > 10000/2 || o.s.length < 2 ? o.s.unshift(' ') : o.s.pop();
+            o.g();
+        }, 65),
+    's': new Array(40).fill(' '),
+    'c': [31, 33, 32, 36, 34, 35],
+}, t = setTimeout){
+    o.g();
+    o.r();
+})();
