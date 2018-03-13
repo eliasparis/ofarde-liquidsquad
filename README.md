19576e871cfae1bc0be178019de484605b2e20ac
Elias Pintos <elias.pintos@tecnilogica.com>
2018-03-13 15:06:41 +0100

    Readme ok

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ofarde-liquidsquad
 OFuscado/ARtístico/DEgenerativo code contest @Liquid Squad
+
+### Execution instructions
+
+* Node (>6.10.0) installed 
+* Terminal with a dark background color
+* Run the following script and enjoy:
+ `node puke.js`
+
--- puke.js
+++ puke.js
@@ -1,18 +1,18 @@
 (function(o = {
     'r': () => t(() => {
-            let m = o.c.reduce((pp, nn) => `${pp}\x1b[${nn}m█████`, '');
-            let p = o.s.join('');
-            console.log(`${p}\x1b[1m${m}\x1b[0m\x1b[39m`);
-            o.r();
-        }, 26),
-    'g': () => t(()=> {
-            let v = Math.floor(Math.random() * (10000 - 1)) + 1;
-            v > 10000/2 || o.s.length < 2 ? o.s.unshift(' ') : o.s.pop();
-            o.g();
-        }, 65),
+        let m = o.c.reduce((pp, nn) => `${pp}\x1b[${nn}m█████`, '');
+        let p = o.s.join('');
+        console.log(`${p}\x1b[1m${m}\x1b[0m\x1b[39m`);
+        o.r();
+    }, 26),
+    'g': () => t(() => {
+        let v = Math.floor(Math.random() * (10000 - 1)) + 1;
+        v > 10000 / 2 || o.s.length < 2 ? o.s.unshift(' ') : o.s.pop();
+        o.g();
+    }, 48),
     's': new Array(40).fill(' '),
     'c': [31, 33, 32, 36, 34, 35],
-}, t = setTimeout){
+}, t = setTimeout) {
     o.g();
     o.r();
 })();
